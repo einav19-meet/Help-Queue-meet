5c6bcecd45f4fe8c349488430bfccc4e7ff80141
einav19-meet <einavstory@gmail.com>
2019-11-09 20:18:37 +0200

    s

--- app.py
+++ app.py
@@ -20,21 +20,38 @@ def login():
             msg = "login failed: username and password does not match"
     return render_template('login.html', msg = msg)
 
+start_time = 0
 @app.route('/home', methods = ['GET', 'POST'])
 def home():
+    global start_time
     if 'username' in login_session:
         username = login_session['username']
         staff = False
         if get_user(username).role != 'student':
             staff = True
+        if start_time != 0:
+            now = str(datetime.datetime.now() - start_time)[:-7]
+        else:
+            now = 0
+        if len(get_student_reqs(username)) == 0:
+            now = 0
+            
+        
+        
+       
+
         if request.method == 'POST':
             question = request.form['question']
             add_request(username, question)
+            if len(get_student_reqs(username)) != 0 and start_time == 0:
+                print("it is")
+                start_time = datetime.datetime.now()
+
 
 
         amount = len(get_reqs())
 
-        return render_template('index2.html',username = username, reqs = get_student_reqs(username), amount = amount, staff = staff)
+        return render_template('index2.html',username = username, now = now, reqs = get_student_reqs(username), amount = amount, staff = staff)
     else:
         return redirect(url_for('login'))
 
@@ -55,6 +72,11 @@ def view():
             return redirect(url_for('home'))
     else:
         return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    login_session.pop('username', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
